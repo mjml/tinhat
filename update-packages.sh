b1e434ccfd981d68626e88b5e8cc9f46b91c62d9
root <mike@michaeljoya.com>
2020-03-27 20:39:28 -0700

    now parameterized by fc version

--- update-packages.sh
+++ update-packages.sh
@@ -2,5 +2,6 @@
 
 source ./setpaths.sh
 
-rpm-tinhat -qa | sed -r -e "s/(\-[0-9a-zA-Z\.]+)?\-[0-9a-zA-Z\.]+\.${FCVER}([\.\-][0-9]+)?\.[a-zA-Z0-9_]+\$//" | sort > package-list.txt
+echo "Writing package list from filesystem at $RESCUEROOT to packages.txt"
+rpm-tinhat -qa | sed -r -e "s/(\-[0-9a-zA-Z\.]+)?\-[0-9a-zA-Z\.]+\.${FCVER}([\.\-][0-9]+)?\.[a-zA-Z0-9_]+\$//" | sort > packages.txt
 
