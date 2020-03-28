21b85105e5a1fd505e55f79cdcb10b984ed5cba7
root <mike@michaeljoya.com>
2020-03-27 20:00:44 -0700

    typo

--- adjust-selinux.sh
+++ adjust-selinux.sh
@@ -2,9 +2,15 @@
 
 . ./setpaths.sh
 
+echo Ensure that SELINUX is set to permissive...
+set -x
 pushd $RESCUEROOT/etc/selinux
-
 sed -e 's/SELINUX=enforcing/SELINUX=permisive/' <config >tmp
 mv -f tmp config
-
 popd
+set +x
+
+echo Relabel everything on first boot...
+set -x
+touch $RESCUEROOT/.autorelabel
+set +x
