003262b6f4ae3be263ff705f5b5264e8e2d11f54
root <mike@michaeljoya.com>
2020-04-21 16:54:52 -0700

    uses --removable on grub-install and also removes the old bootloader that dnf would have installed

--- makebootloader.sh
+++ makebootloader.sh
@@ -4,14 +4,20 @@ source ./setpaths.sh
 
 set -x
 
-echo "Automatically creating $RESCUEBOOT/grub2/grub.cfg"
-grub2-mkconfig -o $RESCUEBOOT/grub2/grub.cfg
+if [[ ! -e $RESCUEBOOT/grub2/grub.cfg ]]; then
+    echo "Automatically creating $RESCUEBOOT/grub2/grub.cfg"
+    grub2-mkconfig -o $RESCUEBOOT/grub2/grub.cfg
+fi
+
+echo Clearing old bootloader
+rm -rf $RESCUEBOOT/efi/*
 
 echo "Installing grub to $RESCUEBOOT on ${RESCUEDEV}1"
-grub2-install --debug --recheck --no-floppy --no-nvram --target=x86_64-efi --efi-directory=$RESCUEBOOT ${RESCUEDEV}1
+grub2-install --debug --removable --recheck --no-floppy --no-nvram --target=x86_64-efi \
+              --efi-directory=$RESCUEBOOT \
+              --boot-directory=$RESCUEBOOT \
+              ${RESCUEDEV}1
 
 echo Fixup to get the correct name for the EFI bootloader...
-mkdir -p $RESCUEBOOT/efi/boot
-rm -rf $RESCUEBOOT/efi/boot/*
 cp $RESCUEBOOT/efi/grub/grubx64.efi $RESCUEBOOT/efi/boot/bootx64.efi
 rm -rf $RESCUEBOOT/efi/grub
